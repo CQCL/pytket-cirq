9d000f279a6cc81dba5401aaf81a0ffb11181c83
Melf <cqc@melf.de>
2023-10-17 10:46:51 +0100

    update pyktet version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.19",
+        "pytket ~= 1.21",
         "cirq-core ~= 1.0",
         "cirq-google ~= 1.0",
         "protobuf ~= 3.20, < 4.0",
